340993bb1baa975ef65fd5dbe277a7f693c22566
axxving <axxving@gmail.com>
2025-07-21 15:59:35 -0600

    chore(deps): dependency update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppRouter } from './routes/AppRouter';
-import { AuthTest } from './components/AuthTest';
 
 export const App = () => {
     return <AppRouter />;
